5bff28ce3341e949593c971a67347d252aee9d11
Michael Jennings <michael.jennings91@gmail.com>
2016-08-15 16:26:36 +0100

    Add variant columns to the metas table

--- database/migrations/2015_05_11_115700_create_metas_table.php
+++ database/migrations/2015_05_11_115700_create_metas_table.php
@@ -19,8 +19,8 @@ class CreateMetasTable extends Migration {
             $table->string('metable_type')->nullable();
             $table->string('identifier')->nullable()->index();
             $table->boolean('default')->default(false);
-            $table->unsignedInteger('groupable_id')->nullable();
-            $table->string('groupable_type')->nullable();
+            $table->unsignedInteger('variant_id')->nullable();
+            $table->string('variant_type')->nullable();
             $table->timestamps();
         });
     }
