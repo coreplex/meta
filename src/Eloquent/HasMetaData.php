b65d1c68d473ede4f3f2132ba6ddf1f570a20ef6
Michael Jennings <michael.jennings91@gmail.com>
2016-08-16 15:24:50 +0100

    Add hasMeta and getMeta methods to the HasMetaData trait

--- src/Eloquent/HasMetaData.php
+++ src/Eloquent/HasMetaData.php
@@ -22,4 +22,31 @@ trait HasMetaData
                     ->where('variant_id', $variant->getKey())
                     ->where('variant_type', $variant->getType());
     }
+
+    /**
+     * Check if the meta has been set for the metable item, if a variant is
+     * passed then check if it is set for the variant.
+     *
+     * @param Variant|null $variant
+     * @return bool
+     */
+    public function hasMeta(Variant $variant = null)
+    {
+        return $this->meta($variant)->exists();
+    }
+
+    /**
+     * Get the meta data for the metable item.
+     *
+     * @param Variant|null $variant
+     * @return mixed
+     */
+    public function getMeta(Variant $variant = null)
+    {
+        if ($variant) {
+            return $this->meta($variant)->getResults();
+        }
+
+        return $this->meta;
+    }
 }
